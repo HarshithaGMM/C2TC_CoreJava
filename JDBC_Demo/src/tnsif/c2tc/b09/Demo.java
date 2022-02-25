2d15e00b51a5b47c7f6e3a8f946a06a554a333b0
Shreyas <Shreyas@ThinBook_1310>
2022-02-25 13:40:00 +0530

    JDBC query

--- JDBC_Demo/src/tnsif/c2tc/b09/Demo.java
+++ JDBC_Demo/src/tnsif/c2tc/b09/Demo.java
@@ -1,21 +1,28 @@
 package tnsif.c2tc.b09;
-import java.sql.*;
+import java.sql.*; //import the package
+import java.util.Scanner;
 public class Demo {
 
 	public static void main(String[] args) throws Exception {
+		Scanner sc=new Scanner(System.in);
+		int UID=sc.nextInt();//2100806
+		String Name=sc.nextLine();//Jaswanth
 		String url="jdbc:mysql://localhost:3306/b09";
 		String user="root";
 		String pass="H##99k@qirt";
-		String query="SELECT * FROM b09.details where UID=2100800;";
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con=DriverManager.getConnection(url,user,pass);
-		Statement st=con.createStatement();
-		ResultSet rs= st.executeQuery(query);
-		rs.next();
-		System.out.println("UID of the Student is="+rs.getInt(1));
-		System.out.println("Name of the Student is="+rs.getString(2));
+		System.out.println(UID);
+		System.out.println(Name);
+		//String query="INSERT INTO `b09`.`details` VALUES"
+		//	+"("+UID+", "+ Name+");";
+		String query="DELETE FROM `b09`.`details` WHERE"
+				+"("+UID+", "+Name+");"; 
+		Class.forName("com.mysql.cj.jdbc.Driver"); //Resister the driver
+		Connection con=DriverManager.getConnection(url,user,pass);// get connection
+		Statement st=con.createStatement();// Create statement
+		int rs= st.executeUpdate(query);//  Execute query
+		System.out.println(" no of rows updated is "+rs);
 		st.close();
-		con.close();
+		con.close();//Close connection
 
 	}
 
